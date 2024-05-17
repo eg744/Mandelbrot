33a653c8b5906b2346eb3ff05cf29ca2ce29a77b
Elijah Gillit <egillit@gmail.com>
2024-05-17 15:58:51 -0600

    stage canvas.js drawpixel, drawline

--- canvas.js
+++ canvas.js
@@ -27,3 +27,24 @@ function initCanvas(canvasElement, width, height) {
 
 	return canvas;
 }
+
+function setCanvasStartParams() {
+	ctx.setTransform(1, 0, 0, 1, 0, 0);
+	ctx.translate(0.5, 0.5);
+}
+
+// Draw pixel on canvas ctx. Caches last color used.
+function drawPixel(x, y, c) {
+	if (lastColor !== c) {
+		lastColor = c;
+		ctx.fillStyle = color[c];
+	}
+	ctx.fillRect(x, y, 1, 1);
+}
+
+function drawLine(x0, y0, x1, y1) {
+	ctx.beginPath();
+	ctx.moveTo(x0, y0);
+	ctx.lineTo(x1, y1);
+	ctx.stroke();
+}
