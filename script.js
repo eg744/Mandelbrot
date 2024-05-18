81d3e93eedf5a3d02ebec0f86c3f50e3921b4af2
Elijah Gillit <egillit@gmail.com>
2024-05-18 16:39:12 -0600

    script.js drawpixel, resetmandelbrot, resetcontrolcolors, computecolors

--- script.js
+++ script.js
@@ -49,3 +49,64 @@ function textOut(s, x, y) {
 	ctx.fillStyle = '#eeb';
 	ctx.fillText(s, x, y);
 }
+
+function reportCoordsAndTiming(msg) {
+	textOut(msg, 4, 4);
+	textOut('x: ' + pMin + '...' + pMax, 4, 16);
+	textOut('y: ' + qMin + '...' + qMax, 4, 28);
+}
+
+// Draws pixel on canvas context. Caches last color.
+function drawPixel(x, y, c) {
+	let currentOffset = 4 * (y * canvasWidth + x);
+
+	// Red
+	mandelbrotPixels[currentOffset] = colors[c][0];
+
+	// Green
+	mandelbrotPixels[currentOffset + 1] = colors[c][1];
+
+	// Blue
+	mandelbrotPixels[currentOffset + 2] = colors[c][2];
+
+	mandelbrotPixels[currentOffset + 3] = 255;
+}
+
+function resetMaldelbrot(w, h) {
+	let scale = 3.0 / h;
+
+	pMin = -(9.0 / 12.0) * w * scale;
+	pMax = (3.0 / 12.0) * w * scale;
+	qMin = -1.5;
+	qMax = 1.5;
+}
+
+function resetControlColors() {
+	controlColors[0] = [0x00, 0x00, 0x20];
+	controlColors[1] = [0xff, 0xff, 0xff];
+	controlColors[2] = [0x00, 0x00, 0xa0];
+	controlColors[3] = [0x40, 0xff, 0xff];
+	controlColors[4] = [0x20, 0x20, 0xff];
+}
+
+function computeColors() {
+	colors[0] = [0, 0, 0];
+
+	for (let i = 0; i < MAX_CONTROL_COLORS - 1; i++) {
+		let rStep = (controlColors[i + 1][0] - controlColors[i][0]) / 63;
+		let gStep = (controlColors[i + 1][1] - controlColors[i][1]) / 63;
+		let bStep = (controlColors[i + 1][2] - controlColors[i][2]) / 63;
+
+		for (let j = 0; j < 64; j++) {
+			colors[j + i * 64 + 1] = [
+				Math.round(controlColors[i][0] + rStep * j),
+				Math.round(controlColors[i][1] + gStep * j),
+				Math.round(controlColors[i][2] + bStep * j),
+			];
+		}
+	}
+
+	for (let i = 257; i < MAX_COLORS; i++) {
+		colors[i] = colors[i - 256];
+	}
+}
