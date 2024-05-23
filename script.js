3a09b1c24569cd9df2ed983cdeab9f53a6d1a9ac
Elijah Gillit <egillit@gmail.com>
2024-05-22 21:34:16 -0600

    functions getcolor, drawmandelbrot

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ class ComplexNumber {
 		this.imaginary = imaginary;
 	}
 
+	// Add complex numbers
 	add(number) {
 		return new ComplexNumber(
 			this.real + number.real,
@@ -11,6 +12,7 @@ class ComplexNumber {
 		);
 	}
 
+	// Multiply complex numbers
 	multiply(number) {
 		return new ComplexNumber(
 			this.real * number.real - this.imaginary * number.imaginary,
@@ -18,6 +20,7 @@ class ComplexNumber {
 		);
 	}
 
+	// Magnitude
 	modulus() {
 		return Math.sqrt(
 			this.real * this.real + this.imaginary * this.imaginary
@@ -25,6 +28,60 @@ class ComplexNumber {
 	}
 }
 
-// function computeMandelbrot(c){
-//     const
+// function isInSet(c){
+
 // }
+
+document.addEventListener('DOMContentLoaded', () => {
+	const canvas = document.getElementById('canvasElement');
+	const ctx = canvas.getContext('2d');
+
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	const width = canvas.width;
+	const height = canvas.height;
+
+	function getColor(iterations, maxIterations) {
+		if (iterations === maxIterations) {
+			return 'black';
+		}
+		const hue = (iterations / maxIterations) * 360;
+		return `hsl(${hue}, 100%, 50%)`;
+	}
+
+	function drawMandelbrot() {
+		const maxIterations = 100;
+
+		// X axis range: [-2.5,1]
+		const xMin = -2.5;
+		const xMax = 1;
+
+		// Y axis range: [-1,1]
+		const yMin = -1;
+		const yMax = 1;
+
+		for (let x = 0; x < canvas.width; x++) {
+			for (let y = 0; y < canvas.height; y++) {
+				const c = new ComplexNumber(
+					xMin + (x / canvas.width) * (xMax - xMin),
+					yMin + (y / canvas.height) * (yMax - yMin)
+				);
+
+				let z = new ComplexNumber(0, 0);
+
+				let iterations = 0;
+
+				// z = z^2 + c
+				while (z.modulus() <= 2 && iterations < maxIterations) {
+					z = z.multiply(z).add(c);
+					iterations++;
+				}
+
+				const color = getColor(iterations, maxIterations);
+				ctx.fillStyle = color;
+				ctx.fillRect(x, y, 1, 1);
+			}
+		}
+	}
+	drawMandelbrot();
+});
